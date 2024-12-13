33b5664558512df921abdc96bc252ce5a33347f8
Joe Winter <joewinteruk@gmail.com>
2024-12-13 12:05:07 +0000

    added tests for user model

--- backend/src/__tests__/models/user.test.ts
+++ backend/src/__tests__/models/user.test.ts
@@ -1,15 +1,51 @@
-import "../../mongodb_helper"
-import User from "../../models/user"
+import "../../mongodb_helper";
+import User from "../../models/user";
 
 describe("User Model", () => {
   beforeEach(async () => {
-    await User.deleteMany({})
-  })
+    await User.deleteMany({});
+  });
 
   it("has an email", () => {
     const user = new User({
-      email: "someone@example.com"
-    })
-    expect(user.email).toEqual("someone@example.com")
-  })
-})
+      email: "someone@example.com",
+    });
+    expect(user.email).toEqual("someone@example.com");
+  });
+  it("has a password", () => {
+    const user = new User({
+      password: "password123",
+    });
+    expect(user.password).toEqual("password123");
+  });
+  it("has a firstname", () => {
+    const user = new User({
+      firstname: "Joe",
+    });
+    expect(user.firstname).toEqual("Joe");
+  });
+  it("has a lastname", () => {
+    const user = new User({
+      lastname: "Winter",
+    });
+    expect(user.lastname).toEqual("Winter");
+  });
+  it("can list all users", async () => {
+    const users = await User.find();
+    expect(users).toEqual([]);
+  });
+  it("can save a user", async () => {
+    const user = new User({
+      email: "someone@example.com",
+      password: "password123",
+      firstName: "chris",
+      lastName: "marion",
+    });
+
+    await user.save();
+    const users = await User.find();
+
+    expect(users[0].email).toEqual("someone@example.com");
+    expect(users[0].password).toEqual("password123");
+  });
+});
--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  email: {type: String}
+  email: {type: String},
+  password: {type: String},
+  firstname: {type: String},
+  lastname: {type: String},
 })
 
 const User = mongoose.model("User", UserSchema)
