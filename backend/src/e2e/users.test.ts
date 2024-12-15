a11743d1c6a5aca16833f2d51b1f5f54a9703daa
Joe Winter <joewinteruk@gmail.com>
2024-12-15 14:31:04 +0000

    create users handler

--- backend/src/e2e/users.test.ts
+++ backend/src/e2e/users.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import { createApp } from "../createApp";
 import { Express } from "express";
+import User from "../models/user";
 
 describe("/users", () => {
   let app: Express = createApp();
@@ -19,8 +20,23 @@ describe("/users", () => {
           firstname: "Joe",
           lastname: "Winter",
         });
+        
+        expect(response.statusCode).toEqual(201)
+      });
+      
+      it("should create a user", async () => {
+      const response = await request(app)
+        .post("/users")
+        .send({
+          email: "someone@example.com",
+          password: "password123",
+          firstname: "Joe",
+          lastname: "Winter",
+        });
 
-      expect(response.statusCode).toEqual(201)
-    });
+      const users = await User.find()
+      const newUser = users[users.length - 1]
+      expect(newUser.email).toEqual("someone@example.com")
+    })
   });
 });
