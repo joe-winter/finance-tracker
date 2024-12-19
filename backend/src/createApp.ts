188a718aa04caaf427c615f9f33ec68685fb1f0e
Joe Winter <joewinteruk@gmail.com>
2024-12-19 20:57:24 +0000

    e2e login route is working

--- backend/src/createApp.ts
+++ backend/src/createApp.ts
@@ -2,12 +2,12 @@ import express from "express";
 import userRouter from "./routes/users";
 import authenticationRouter from "./routes/authentication"
 import bodyParser from "body-parser";
-
+import cors from "cors";
 export function createApp() {
   const app = express();
 
   app.use(bodyParser.json())
-
+  app.use(cors())
   app.use("/users", userRouter);
   app.use("/tokens", authenticationRouter)
 
--- backend/src/lib/token.ts
+++ backend/src/lib/token.ts
@@ -1,18 +1,26 @@
 import JWT from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 const secret = process.env.JWT_SECRET as string;
 
 /**
  * This function is used to generate a JWT authentication
  * token for a specific user.
  * JWTs in 100 Seconds: https://www.youtube.com/watch?v=UBUNrFtufWo
- */
+*/
 function generateToken(user_id: string): string {
+  
+  console.log(secret)
+  // if (!secret) {
+  //   throw new Error ("JWT_SECRET is not defined in environment variables")
+  // }
   if (user_id) {
     return JWT.sign(
       {
         user_id: user_id,
         iat: Math.floor(Date.now() / 1000),
-
+        
         // Set the JWT token to expire in 10 minutes
         exp: Math.floor(Date.now() / 1000) + 10 * 60,
       },
