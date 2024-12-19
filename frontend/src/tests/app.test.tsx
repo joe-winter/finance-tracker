eea2acc283b9887d5b2f81e39c1486bf9406988d
Joe Winter <joewinteruk@gmail.com>
2024-12-19 14:18:35 +0000

    set up react with typescript and vite]

---
+++ frontend/src/tests/app.test.tsx
@@ -0,0 +1,8 @@
+
+import { describe, expect, it } from "vitest"
+
+describe("app", () => {
+  it("should be true", () => {
+    expect(true).toBeTruthy()
+  })
+})
---
+++ frontend/src/tests/pages/login.test.tsx
@@ -0,0 +1,14 @@
+import { describe, expect, it } from "vitest";
+import {render, screen} from "@testing-library/react"
+import Login from "../../pages/Login";
+import '@testing-library/jest-dom'
+
+describe("Login Page", () => {
+  it("should render heading", () => {
+    render(<Login/>)
+
+    const headingEl = screen.getByRole("heading", {name: "Login"})
+
+    expect(headingEl).toBeInTheDocument()
+  })
+})
