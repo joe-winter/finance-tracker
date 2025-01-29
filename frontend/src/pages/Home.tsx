7f151a259ad91114f545e20006ce963558ba1b93
Joe Winter <joewinteruk@gmail.com>
2025-01-29 14:33:45 +0000

    sylted hoem and login page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,18 +1,31 @@
 import { useNavigate } from "react-router-dom";
 
 export default function Home() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleLogInClick = () => {
-    navigate("/login")
-  }
+    navigate("/login");
+  };
   const handleSignUpClick = () => {
-    navigate("/signup")
-  }
+    navigate("/signup");
+  };
   return (
-    <div className="h-screen flex items-center justify-center space-x-4">
-      <button onClick={handleLogInClick} className="bg-gray-800 hover:bg-gray-700 text-white font-medium py-2 px-4 rounded transition">Log In</button>
-      <button onClick={handleSignUpClick} className="bg-gray-800 hover:bg-gray-700 text-white font-medium py-2 px-4 rounded transition">Sign Up</button>
+    <div className="h-screen flex flex-col items-center justify-center">
+      <h1 className="text-2xl font-medium mb-6">Finance Tracker</h1>
+      <div className="flex space-x-4">
+        <button
+          onClick={handleLogInClick}
+          className="bg-gray-800 hover:bg-gray-700 text-white font-medium py-2 px-4 rounded transition"
+        >
+          Log In
+        </button>
+        <button
+          onClick={handleSignUpClick}
+          className="bg-gray-800 hover:bg-gray-700 text-white font-medium py-2 px-4 rounded transition"
+        >
+          Sign Up
+        </button>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,7 +2,7 @@ import LoginForm from "../components/LoginForm"
 
 export default function Login() {
   return (
-    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto">
+    <div className="h-screen flex flex-col items-center justify-center px-6 py-8 mx-auto lg:max-w-lg md:max-w-md sm:max-w-sm">
       <h2 className="mb-6 text-xl font-semibold text-gray-900">Login</h2>
       <LoginForm />
     </div>
