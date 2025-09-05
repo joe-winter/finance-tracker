daa845bc95671bf1f568baced3f91bf3e2f20b66
Joe Winter <joewinteruk@gmail.com>
2025-09-05 09:45:35 +0100

    fix: deploy

--- app/components/finances/new-dialog.tsx
+++ app/components/finances/new-dialog.tsx
@@ -13,241 +13,241 @@ import { cn } from "@/lib/utils";
 import { Button } from "../ui/button";
 import { Calendar } from "../ui/calendar";
 import {
-	Dialog,
-	DialogClose,
-	DialogContent,
-	DialogDescription,
-	DialogFooter,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "../ui/dialog";
 import {
-	Form,
-	FormControl,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "../ui/form";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import {
-	Select,
-	SelectContent,
-	SelectGroup,
-	SelectItem,
-	SelectLabel,
-	SelectTrigger,
-	SelectValue,
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
 } from "../ui/select";
 import { Textarea } from "../ui/textarea";
 
 const formSchema = z.object({
-	amount: z
-		.string()
-		.regex(/^(0|[1-9]\d*)\.\d{1,2}$/, {
-			message: "Must be a number with exactly 2 decimal places",
-		})
-		.refine((val) => parseFloat(val) > 0, {
-			message: "Money amount must be greater than 0",
-		}),
-	date: z.date(),
-	description: z.string().min(1).max(500),
-	categoryId: z.string(),
+  amount: z
+    .string()
+    .regex(/^(0|[1-9]\d*)\.\d{1,2}$/, {
+      message: "Must be a number with exactly 2 decimal places",
+    })
+    .refine((val) => parseFloat(val) > 0, {
+      message: "Money amount must be greater than 0",
+    }),
+  date: z.date(),
+  description: z.string().min(1).max(500),
+  categoryId: z.string(),
 });
 
 export const dateToString = (date: Date | undefined): string => {
-	if (!date) return "";
-	if (!isValid(date)) {
-		throw new Error("Invalid date provided");
-	}
-	return format(date, "yyyy-MM-dd");
+  if (!date) return "";
+  if (!isValid(date)) {
+    throw new Error("Invalid date provided");
+  }
+  return format(date, "yyyy-MM-dd");
 };
 
 type FormSchema = z.infer<typeof formSchema>;
 
 export default function NewDialog() {
-	const [open, setOpen] = useState(false);
-	const [calendarOpen, setCalendarOpen] = useState(false);
-	const utils = trpc.useUtils();
-	const getCategories = trpc.category.getCategories.useQuery();
-	const createMutation = trpc.transaction.createTransaction.useMutation();
-	const form = useForm({
-		resolver: zodResolver(formSchema),
-		defaultValues: {
-			amount: "",
-			date: new Date(),
-			description: "",
-		},
-	});
+  const [open, setOpen] = useState(false);
+  const [calendarOpen, setCalendarOpen] = useState(false);
+  const utils = trpc.useUtils();
+  const getCategories = trpc.category.getCategories.useQuery();
+  const createMutation = trpc.transaction.createTransaction.useMutation();
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      amount: "",
+      date: new Date(),
+      description: "",
+    },
+  });
 
-	const handleSubmit = (data: FormSchema) => {
-		console.log(data.date);
-		console.log(data.date.toDateString());
-		console.log(data.date.toISOString());
-		console.log(data.date.toString());
-		console.log(format(data.date, "PPP mm:HH"));
-		createMutation.mutate(data, {
-			onSettled: () => {
-				utils.transaction.getTransactions.invalidate();
-				form.reset();
-				setOpen(false);
-			},
-		});
-	};
-	return (
-		<Dialog open={open} onOpenChange={setOpen}>
-			<DialogTrigger asChild>
-				<Button variant="outline">
-					Add New <PlusIcon className="size-4" />
-				</Button>
-			</DialogTrigger>
-			<DialogContent className="sm:max-w-[425px]">
-				<DialogHeader>
-					<DialogTitle>Add transaction</DialogTitle>
-					<DialogDescription>
-						Add your transaction here, empty descriptions will be set as the
-						category
-					</DialogDescription>
-				</DialogHeader>
-				<Form {...form}>
-					<form
-						onSubmit={form.handleSubmit(handleSubmit)}
-						className="grid gap-4"
-					>
-						<FormField
-							control={form.control}
-							name="amount"
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Amount</FormLabel>
-									<FormControl>
-										<CurrencyInput
-											name={field.name}
-											className={cn(
-												"flex h-9 w-full min-w-0 rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-xs outline-none transition-[color,box-shadow] selection:bg-primary selection:text-primary-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:font-medium file:text-foreground file:text-sm placeholder:text-muted-foreground disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm dark:bg-input/30",
-												"focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50",
-												"aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40",
-											)}
-											value={field.value}
-											onValueChange={field.onChange}
-											onBlur={field.onBlur}
-											placeholder="Enter amount"
-											prefix="£"
-											step={0.01}
-											decimalsLimit={2}
-											allowNegativeValue={false}
-											decimalScale={2}
-										/>
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-						<FormField
-							control={form.control}
-							name="date"
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Date</FormLabel>
-									<Popover open={calendarOpen} onOpenChange={setCalendarOpen}>
-										<PopoverTrigger asChild>
-											<FormControl>
-												<Button
-													variant={"outline"}
-													className={cn(
-														"pl-3 text-left font-normal",
-														!field.value && "text-muted-foreground",
-													)}
-												>
-													{field.value ? (
-														format(field.value, "PPP")
-													) : (
-														<span>Pick a date</span>
-													)}
-													<CalendarIcon className="ml-auto size-4 opacity-50" />
-												</Button>
-											</FormControl>
-										</PopoverTrigger>
-										<PopoverContent className="w-auto p-0" align="start">
-											<Calendar
-												mode="single"
-												selected={new Date(field.value)}
-												onSelect={(value) => {
-													field.onChange(value);
-													setCalendarOpen(false);
-												}}
-												captionLayout="dropdown"
-											/>
-										</PopoverContent>
-									</Popover>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-						<FormField
-							control={form.control}
-							name="categoryId"
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>Category</FormLabel>
-									<Select
-										onValueChange={field.onChange}
-										defaultValue={field.value}
-									>
-										<FormControl>
-											<SelectTrigger className="w-full capitalize">
-												<SelectValue placeholder="Select" />
-											</SelectTrigger>
-										</FormControl>
-										<SelectContent>
-											{Object.values(TransactionType).map((type) => (
-												<SelectGroup key={type}>
-													<SelectLabel className="capitalize">
-														{type.toLowerCase()}
-													</SelectLabel>
-													{getCategories.data
-														?.filter((category) => category.type === type)
-														.map((category) => (
-															<SelectItem key={category.id} value={category.id}>
-																{category.name}
-															</SelectItem>
-														))}
-												</SelectGroup>
-											))}
-										</SelectContent>
-									</Select>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-						<FormField
-							control={form.control}
-							name="description"
-							render={({ field }) => (
-								<FormItem className="size-full">
-									<FormLabel>Description</FormLabel>
-									<FormControl>
-										<Textarea placeholder="..." className="h-full" {...field} />
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
+  const handleSubmit = (data: FormSchema) => {
+    console.log(data.date);
+    console.log(data.date.toDateString());
+    console.log(data.date.toISOString());
+    console.log(data.date.toString());
+    console.log(format(data.date, "PPP mm:HH"));
+    createMutation.mutate(data, {
+      onSettled: () => {
+        utils.transaction.getTransactions.invalidate();
+        form.reset();
+        setOpen(false);
+      },
+    });
+  };
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button variant="outline">
+          Add New <PlusIcon className="size-4" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add transaction</DialogTitle>
+          <DialogDescription>
+            Add your transaction here, empty descriptions will be set as the
+            category
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className="grid gap-4"
+          >
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Amount</FormLabel>
+                  <FormControl>
+                    <CurrencyInput
+                      name={field.name}
+                      className={cn(
+                        "flex h-9 w-full min-w-0 rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-xs outline-none transition-[color,box-shadow] selection:bg-primary selection:text-primary-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:font-medium file:text-foreground file:text-sm placeholder:text-muted-foreground disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm dark:bg-input/30",
+                        "focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50",
+                        "aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40"
+                      )}
+                      value={field.value}
+                      onValueChange={field.onChange}
+                      onBlur={field.onBlur}
+                      placeholder="Enter amount"
+                      prefix="£"
+                      step={0.01}
+                      decimalsLimit={2}
+                      allowNegativeValue={false}
+                      decimalScale={2}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="date"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Date</FormLabel>
+                  <Popover open={calendarOpen} onOpenChange={setCalendarOpen}>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant={"outline"}
+                          className={cn(
+                            "pl-3 text-left font-normal",
+                            !field.value && "text-muted-foreground"
+                          )}
+                        >
+                          {field.value ? (
+                            format(field.value, "PPP")
+                          ) : (
+                            <span>Pick a date</span>
+                          )}
+                          <CalendarIcon className="ml-auto size-4 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={(value) => {
+                          field.onChange(value);
+                          setCalendarOpen(false);
+                        }}
+                        captionLayout="dropdown"
+                      />
+                    </PopoverContent>
+                  </Popover>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="categoryId"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Category</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger className="w-full capitalize">
+                        <SelectValue placeholder="Select" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {Object.values(TransactionType).map((type) => (
+                        <SelectGroup key={type}>
+                          <SelectLabel className="capitalize">
+                            {type.toLowerCase()}
+                          </SelectLabel>
+                          {getCategories.data
+                            ?.filter((category) => category.type === type)
+                            .map((category) => (
+                              <SelectItem key={category.id} value={category.id}>
+                                {category.name}
+                              </SelectItem>
+                            ))}
+                        </SelectGroup>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem className="size-full">
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Textarea placeholder="..." className="h-full" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-						<DialogFooter>
-							<DialogClose asChild>
-								<Button variant="outline" type="button">
-									Cancel
-								</Button>
-							</DialogClose>
-							<Button type="submit" disabled={createMutation.isPending}>
-								Confirm
-							</Button>
-						</DialogFooter>
-					</form>
-				</Form>
-			</DialogContent>
-		</Dialog>
-	);
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="outline" type="button">
+                  Cancel
+                </Button>
+              </DialogClose>
+              <Button type="submit" disabled={createMutation.isPending}>
+                Confirm
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
 }
