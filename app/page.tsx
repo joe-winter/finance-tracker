6b343e7c9cd7d6f7aef2758597e455a5808e909b
Joe Winter <joewinteruk@gmail.com>
2025-09-09 22:12:57 +0100

    fix: redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,24 @@
+import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 import { Button } from "./components/ui/button";
 
 export default async function Page() {
-	return (
-		<div className="-mt-16 flex min-h-screen flex-col items-center justify-center">
-			<div className="flex gap-4">
-				<Button asChild>
-					<Link href="/sign-in">Sign In</Link>
-				</Button>
-				<Button asChild variant="outline">
-					<Link href="/sign-up">Sign Up</Link>
-				</Button>
-			</div>
-		</div>
-	);
+  const { userId } = await auth();
+
+  if (userId) {
+    return redirect("/dashboard");
+  }
+  return (
+    <div className="-mt-16 flex min-h-screen flex-col items-center justify-center">
+      <div className="flex gap-4">
+        <Button asChild>
+          <Link href="/sign-in">Sign In</Link>
+        </Button>
+        <Button asChild variant="outline">
+          <Link href="/sign-up">Sign Up</Link>
+        </Button>
+      </div>
+    </div>
+  );
 }
